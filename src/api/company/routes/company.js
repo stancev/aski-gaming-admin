5711ec4f8bedb919ab6144fda7851f219d400575
stanojevicbojan <bojanstanojevic89@gmail.com>
2024-02-04 20:48:09 +0100

    homepage counters update, extended find method for company,reviews to get total ratings and average ratings

--- src/api/company/routes/company.js
+++ src/api/company/routes/company.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * company router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::company.company');
+module.exports = createCoreRouter("api::company.company");
---
+++ src/api/homepage-counter/routes/homepage-counter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * homepage-counter router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::homepage-counter.homepage-counter');
